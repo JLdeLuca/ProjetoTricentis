95b41da6623ebf192ad9e37dc0f5865117af991e
LUCA <rluca@osite.com.br>
2020-12-20 23:24:55 -0300

    Organização das pastas do projeto

---
+++ src/test/java/br/com/luca/stepDefinition/CadasroSteps.java
@@ -0,0 +1,57 @@
+package br.com.luca.stepDefinition;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import br.com.luca.PageObject.PageFormulario;
+import cucumber.api.java.pt.Dado;
+import cucumber.api.java.pt.Entao;
+import cucumber.api.java.pt.Quando;
+
+
+public class CadasroSteps {
+	
+	static WebDriver driver;
+	private PageFormulario pageFormulario;
+
+	@Dado("^que eu acesse o site$")
+	public void queEuAcesseOSite()  {
+		String path = System.getProperty("user.dir");
+		System.getProperty("webdriver.chrome.driver", path.concat("\\Driver_versao87\\Chromedriver.exe"));
+		System.out.println(path.concat("\\Driver_versao87\\Chromedriver.exe"));
+		WebDriver driver = new ChromeDriver();
+		driver.get("http://sampleapp.tricentis.com/101/app.php");
+		driver.manage().timeouts().implicitlyWait(1, TimeUnit.MINUTES);
+		driver.manage().window().maximize();
+		pageFormulario = new PageFormulario(driver);
+	}
+
+	@Quando("^eu realizar o preenchimento dos formularios$")
+	public void euRealizarOPreenchimentoDosFormularios() throws InterruptedException {
+		pageFormulario.pageForm1();
+		pageFormulario.pageForm2();
+		pageFormulario.pageForm3();
+	}
+
+	@Quando("^seleciono o preco$")
+	public void selecionoOPreco() throws InterruptedException {
+		pageFormulario.pageForm4();
+	}
+
+	@Quando("^efetuar o envio das informacoes$")
+	public void efetuarOEnvioDasInformacoes() throws InterruptedException  {
+		pageFormulario.pageForm5();
+
+	}
+
+	@Entao("^eu recebo a mensagem \"([^\"]*)\"$")
+	public void euReceboAMensagem(String string) throws InterruptedException  {
+		assertEquals(pageFormulario.pegaMsg(), string);
+		pageFormulario.fechartudo();
+	}
+
+}
