893c7d9025d5cdf26965714b253efa19f4ebeab4
LUCA <rluca@osite.com.br>
2021-09-15 00:15:56 -0300

    Organização runner

--- src/test/java/runner/RunnerTest.java
+++ src/test/java/runner/RunnerTest.java
@@ -13,12 +13,16 @@ import cucumber.api.SnippetType;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = { "src/test/resources/Features/" }, plugin = { "pretty", "html:target/cucumber-reports",
+@CucumberOptions(
+		features = { "src/test/resources/Features/" },
+		plugin = { "pretty", "html:target/cucumber-reports",
 		"json:target/report/cucumber.json", "html:target/report/cucumber.html",
 		"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/report.html" },
 //		"com.cucumber.listener.ExtentCucumberFormatter:target/cucumber-reports/relatorio.html" },
-
-		snippets = SnippetType.CAMELCASE, glue = { "br.com.luca.stepDefinition" }, monochrome = true, tags = "@web")
+		snippets = SnippetType.CAMELCASE,
+		glue = { "br.com.luca.stepDefinition" },
+		monochrome = true,
+		tags = "@web")
 
 public class RunnerTest {
 
